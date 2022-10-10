716f5dbc07fd62c43c8a0ca443c4f2cfb7999ef3
iChris <christianjoseph473@gmail.com>
2022-10-10 08:01:49 -0700

    changed endpoint

--- client/src/components/axiosMethod.js
+++ client/src/components/axiosMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
- const BASE_URL = "https://maxigivez.herokuapp.com/api";
+ const BASE_URL = "https://maxgivez.herokuapp.com/api";
 //const BASE_URL = "http://localhost:8500/api";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
